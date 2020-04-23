33bbdacc48eaf30f80308073d88fa9f87c2559b1
urvashigupta910 <59687473+urvashigupta910@users.noreply.github.com>
2020-04-23 15:31:21 +0800

    Update BrowserFactory.java

--- src/test/java/com/automation/mercury/util/BrowserFactory.java
+++ src/test/java/com/automation/mercury/util/BrowserFactory.java
@@ -3,10 +3,12 @@ package com.automation.mercury.util;
 import java.sql.Driver;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BrowserFactory {
 	private static WebDriver driver=null;
@@ -17,6 +19,8 @@ public class BrowserFactory {
 		{
 			String os = System.getProperty("os.name").toLowerCase();
 			System.out.println("OS = " + os );
+			if (os.equalsIgnoreCase("windows 10"))
+			{
 			if(rm.equalsIgnoreCase("remote"))
 			{
 				
@@ -27,7 +31,11 @@ public class BrowserFactory {
 				{
 					System.setProperty("webdriver.chrome.driver", "./BrowserExe/chromedriver.exe");
 					BrowserFactory.driver=new ChromeDriver();
-					System.out.println();
+					Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
+					String browserName = caps.getBrowserName();
+					  String browserVersion = caps.getVersion();
+					  System.out.println("Browser name ::"+ browserName+ "  Browser version ::"+browserVersion);
+					
 					
 				}
 				else if(bname.equalsIgnoreCase("firefox"))
@@ -40,6 +48,38 @@ public class BrowserFactory {
 					System.setProperty("webdriver.ie.driver", "./BrowserExe/IEDriverServer.exe");
 					BrowserFactory.driver=new InternetExplorerDriver();
 				}
+			}
+			}
+			else if(os.equalsIgnoreCase("linux"))
+			{
+				if(rm.equalsIgnoreCase("remote"))
+				{
+					
+				}
+				else if(rm.equalsIgnoreCase("local"))
+				{
+					if(bname.equalsIgnoreCase("chrome"))
+					{
+						System.setProperty("webdriver.chrome.driver", "./BrowserExe/chromedriver");
+						BrowserFactory.driver=new ChromeDriver();
+						 Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
+						  String browserName = caps.getBrowserName();
+						  String browserVersion = caps.getVersion();
+						  System.out.println("Browser name ::"+ browserName+ "  Browser version ::"+browserVersion);
+						
+					}
+					else if(bname.equalsIgnoreCase("firefox"))
+					{
+						System.setProperty("webdriver.gecko.driver", "./BrowserExe/geckodriver.exe");
+						BrowserFactory.driver=new FirefoxDriver();
+					}
+					else if(bname.equalsIgnoreCase("IE"))
+					{
+						System.setProperty("webdriver.ie.driver", "./BrowserExe/IEDriverServer.exe");
+						BrowserFactory.driver=new InternetExplorerDriver();
+					}
+				}
+				
 			}
 		}
 		return BrowserFactory.driver;
